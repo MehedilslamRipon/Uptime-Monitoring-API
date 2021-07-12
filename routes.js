ad6ef40716eb1efa9f0fade9c2db263684251d05
Mehedi Islam Ripon <51530291+MehedilslamRipon@users.noreply.github.com>
2021-07-12 14:14:06 +0600

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,11 +1,3 @@
-/*
- * Title: Routes
- * Description: Application Routes
- * Author: Sumit Saha ( Learn with Sumit )
- * Date: 11/15/2020
- *
- */
-
 // dependencies
 const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
